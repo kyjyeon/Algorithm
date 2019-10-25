93c9b4cb4bcf8be4823be7e4c42e0a40f8474cc9
YJ Kim <39256000+kyjyeon@users.noreply.github.com>
2019-10-25 14:19:29 +0900

    Add files via upload

---
+++ sortedList/main.cpp
@@ -0,0 +1,18 @@
+/**
+*	@mainpage
+*	@date	2019.10.15
+*	@author ±è¿¬ÁØ. 
+*/
+
+#include "Application.h"
+																																					
+/**
+*	program main function for data structures course.
+*/
+int main()
+{
+	Application app;	// Program application
+	app.Run();			// run program
+
+	return 0;
+}
